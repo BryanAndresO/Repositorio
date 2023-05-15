789e7bd4957bf484303f09fa3892ac43154231c7
Bryan Andres <bryanfamiliat@gmail.com>
2023-05-15 10:40:19 -0500

    Lista

--- Lista.py
+++ Lista.py
@@ -1,2 +1,7 @@
-mi_lista = [1, 2, 3, 4, 5]
-print(mi_lista)
+empty_list = []
+languages = ['Python', 'Ruby', 'Javascript']
+fibonacci = [0, 1, 1, 2, 3, 5, 8, 13]
+data = ['Tenerife', {'cielo': 'limpio', 'temp': 24}, 3718, (28.2933947, -16.5226597)]
+my_list = [1, 2, 3]
+my_list.append(4)
+print(my_list) # Output: [1, 2, 3, 4]
